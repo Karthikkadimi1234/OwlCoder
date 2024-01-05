57fcee75b0672c8b9443a5d9b88a9ffce4b2f124
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2024-01-05 21:37:13 +0530

    Update OWLCoderDay5.cpp

--- OWLCoderDay5.cpp
+++ OWLCoderDay5.cpp
@@ -127,9 +127,38 @@ for(int i=0;i<q;i++)
   
 }
 
+//Todays POTD  in GEEKforGEEKS
+#include<bits/stdc++.h>
+using namespace std;
 
+// } Driver Code Ends
+class Solution{
+	public:
+	int TotalWays(int N)
+	{  
+	    long long a=0 ,b=1,temp;
+	    long long mod=1e9+7;
+	    for(int i=0;i<=N;i++){
+	        temp=(a+b)%(mod);
+	        a=b;
+	        b=temp;
+	    }
+	    
+	    return (((temp)%(mod))*((temp)%(mod)))%mod;
+	}
+};
 
-
-
-
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int N;
+		cin >> N;
+		Solution ob;
+		int ans = ob.TotalWays(N);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
 
